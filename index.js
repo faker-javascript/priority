f93794deca97f5505ad300a5ee82dacf5587a839
Sergey Romanenko <sergey.romanenko@flextype.org>
2022-01-23 14:17:34 +0300

    refactor(core): update priorities

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 export default function priority() {
     const priorities = [
-        "low",
-        "medium",
-        "high",
-        "critical",
-        "urgent",
-        "major",
-        "moderate",
-        "minor"
+        'low',
+        'medium',
+        'high',
+        'critical',
+        'urgent',
+        'major',
+        'moderate',
+        'minor',
     ];
     return priorities[Math.floor(Math.random() * priorities.length)];
 }
